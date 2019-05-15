32ed297231eeabeebb32b21a292632bbb04d5407
staplejm <staplejm@dukes.jmu.edu>
2019-05-15 09:52:45 -0400

    Success adding noninteractive functions

--- main.js
+++ main.js
@@ -1,54 +1,4 @@
-class Door extends Room {
-  constructor(name, descriptor, room) {
-    super(name, descriptor);
-    this.keyHole = 'key';
-    this.key = 'rusty key';
-    this.room = room;
-  }
 
-  unlock(key) {
-    if(key.name = this.key) {
-      this.keyHole = key;
-    } else {
-      addLine("The " + key.name + " doesn't fit.");
-    }
-  }
-
-  open(player) {
-    if(this.keyHole != null) {
-
-      this.room.inspect(player);
-
-    } else {
-      addLine("The door is locked.");
-    }
-  }
-
-  enter() {
-    if(this.keyHole != null) {
-      addLine("You find yourself in a " + this.name + ".")
-
-      //Get contents of room
-      let contents = ""
-      if (this.contents.length > 0) {
-        for (var i = 0; i < this.contents.length; i++) {
-          if (i == this.contents.length - 1) {
-            if (i > 0) {
-              contents = contents + " and a " + this.contents[i].name + ".";
-            } else {
-              contents = contents + this.contents[i].name + ".";
-            }
-          } else {
-            contents = contents + this.contents[i].name + ", ";
-          }
-        }
-        addLine("You see a " + contents);
-      } else {
-        addLine("You see nothing")
-      }
-    }
-  }
-}
 
 
 alert("Loading main.js!"); //Don't change this line
@@ -59,24 +9,23 @@ let hallway = new Room("dusty hallway", "clouds of dust kick up with every step.
 let vase = new Item("vase", "made of blue glass, chipped on top. Filled with a dark liquid.")
 let room = new Room("dark room", "It is dark");
 let bedroom = new Room('bedroom', 'It is a bedroom')
-let door = new Door('door', 'It is wooden, and scratched up. The doorknob is made of brass.', room)
 var key = new Item('rusty key', 'It is a large, heavy brass key.')
 
 // Create the function
-let open = function (action, player, object) {
-  object.open(player);
+let jump = function (action, player, object) {
+  addLine('You jump. As you land, you hear a loud creak from the floor.')
   return player
 }
 
 
 //Decide what the player should type to run the function
-let openAction = /open/;
+let jumpaction = /jump/;
 //Add the function to the action list
-addAction(openAction, open);
+addAction(jumpaction, jump);
 
 // Put them in their spots
 hallway.addItem(vase);
-hallway.addItem(door);
+hallway.addItem(room);
 room.addItem(bedroom);
 room.addItem(hallway);
 
